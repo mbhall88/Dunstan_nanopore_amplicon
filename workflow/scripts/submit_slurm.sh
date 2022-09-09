3921966392befca1a21d24e3ab250ec965387fcf
Michael Hall <michael@mbh.sh>
2022-09-09 12:32:13 +1000

    Create submit_slurm.sh

---
+++ workflow/scripts/submit_slurm.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -eux
+
+JOB_NAME="snakemake_master_process."$(date "+%s")
+LOG_DIR="logs"
+
+if [[ ! -d "$LOG_DIR" ]]; then
+    echo "Error: Log directory $LOG_DIR does not exist"
+    exit 1
+fi
+
+MEMORY="2G"
+TIME="3d"
+THREADS=2
+PROFILE="slurm.punim1637"
+SINGULARITY_ARGS="\'--nv\'"
+CMD="snakemake --profile $PROFILE --rerun-incomplete --local-cores $THREADS $* --singularity-args $SINGULARITY_ARGS"
+
+ssubmit -t "$TIME" -m "$MEMORY" -o "$LOG_DIR"/"$JOB_NAME".o \
+    -e "$LOG_DIR"/"$JOB_NAME".e "$JOB_NAME" "$CMD" -- -c "$THREADS"
