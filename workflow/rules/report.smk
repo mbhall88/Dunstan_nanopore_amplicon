eaf262f57aa9a31002936a480688912e068091b6
Michael Hall <michael@mbh.sh>
2022-09-26 15:59:26 +1000

    add plot dst rule

---
+++ workflow/rules/report.smk
@@ -0,0 +1,19 @@
+rule plot_dst:
+    input:
+        summary=RESULTS / "reports/{tool}.csv",
+    output:
+        plot=report(PLOTS / "{tool}.dst.png", category="DST prediction"),
+    log:
+        LOGS / "plot_dst/{tool}.log",
+    resources:
+        time="10m",
+    params:
+        fontsize=14,
+        dpi=300,
+        figsize=(13, 13),
+        palette="Set2",
+        marker_size=300,
+    conda:
+        ENVS / "plot_dst.yaml"
+    script:
+        str(SCRIPTS / "plot_dst.py")
---
+++ workflow/scripts/plot_dst.py
@@ -0,0 +1,107 @@
+import sys
+
+sys.stderr = open(snakemake.log[0], "w")
+
+from collections import Counter
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+plt.style.use("ggplot")
+
+FS = snakemake.params.fontsize
+DPI = snakemake.params.dpi
+FIGSIZE = snakemake.params.figsize
+PALETTE = snakemake.params.palette
+SIZE = snakemake.params.marker_size
+
+
+def main():
+    df = pd.read_csv(snakemake.input.summary)
+    drugs = sorted(set(df["drug"]))
+    preds = list(set(df["prediction"]))
+
+    s = """AMK amikacin
+    CAP capreomycin
+    CFX ciprofloxacin
+    DLM delamanid
+    EMB ethambutol
+    ETO ethionamide
+    INH isoniazid
+    KAN kanamycin
+    LFX levofloxacin
+    LZD linezolid
+    MFX moxifloxacin
+    OFX ofloxacin
+    PZA pyrazinamide
+    RIF rifampicin
+    STM streptomycin"""
+    long2short = dict()
+    for line in s.splitlines():
+        ab, d = line.split()
+        long2short[d.capitalize()] = ab
+
+    short2long = dict()
+    for line in s.splitlines():
+        ab, d = line.split()
+        short2long[ab] = d.capitalize()
+
+    first_line = [short2long[d] for d in ["INH", "RIF", "EMB", "PZA"]]
+    fluoroquinolones = [short2long[d] for d in ["LFX", "MFX", "OFX", "CFX"]]  # group A
+    macrolides = [
+        short2long[d] for d in ["AMK", "CAP", "KAN", "STM"]
+    ]  # group B - second-line injectables
+    other = [short2long[d] for d in ["ETO", "LZD", "DLM"]]  # group C and D
+    drug_order = [
+        d for d in [*first_line, *fluoroquinolones, *macrolides, *other] if d in drugs
+    ]
+
+    def sort_drugs(a):
+        xs = drug_order
+        out = []
+        c = Counter()
+        for x in a:
+            i = xs.index(x)
+            d = xs[i]
+            c[d] += 1
+            out.append((i, c[d]))
+        return out
+
+    df = df.sort_values(by="drug", key=sort_drugs).reset_index(drop=True)
+
+    fig, ax = plt.subplots(figsize=FIGSIZE, dpi=DPI)
+    sns.scatterplot(
+        data=df,
+        x="drug",
+        y="run",
+        hue="prediction",
+        hue_order=sorted(preds, reverse=True),
+        ax=ax,
+        s=SIZE,
+        edgecolor="black",
+        palette=PALETTE,
+    )
+
+    ax.set(xlabel="", ylabel="")
+    xticks = [long2short[d] for d in drug_order]
+    ax.set_xticklabels(xticks, fontsize=FS)
+    ax.xaxis.tick_top()
+    handles, labels = ax.get_legend_handles_labels()
+    pred_lookup = {"R": "Resistant", "r": "Minor resistance", "S": "Susceptible"}
+    labels = [pred_lookup[l] for l in labels]
+    ax.legend(
+        handles=handles,
+        labels=labels,
+        bbox_to_anchor=(0.5, -0.05),
+        ncol=len(preds),
+        prop=dict(size=FS),
+        frameon=False,
+        loc="lower center",
+        markerscale=3,
+    )
+    _ = ax.tick_params(axis="y", which="major", labelsize=FS)
+
+    fig.savefig(snakemake.output.plot)
+
+
+main()
